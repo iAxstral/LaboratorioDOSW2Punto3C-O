0303a0fb2bb8a9d3d1ad5dd4dc02ec633e275ae7
Tomas <tomasdi.dev@gmail.com>
2026-02-07 18:15:52 -0500

    Estructura base Java y setup inicial del Reto 3

---
+++ src/main/java/com/laboratorio/reto3/Application.java
@@ -0,0 +1,8 @@
+package com.laboratorio.reto3;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Reto 3 - Reino de los Vehículos");
+    }
+}
